18a697bec1c60a128ee3173f3ed4b5376012d1f4
郑顺东哒哒哒 <33625419+zhengshundong01@users.noreply.github.com>
2018-11-04 22:38:53 +0800

    z

---
+++ src/renderer/components/common/popup/SeeImg/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fd-popup-seeimg-warpper">
+        <img :src="scope.src" alt="scope.alt" class="fd-popup-seeimg-img">
+        <div class="fd-popup-seeimg-content">
+            <span>see</span>
+            <span>delete</span>
+        </div>
+    </div>
+</template>
+<script src="./index.js" type="text/ecmascript-6"></script>
